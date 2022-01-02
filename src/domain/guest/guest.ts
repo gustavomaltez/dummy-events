2c31e885e08c08cb7add7b5218e645f2104e8c66
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-01-02 11:32:57 -0300

    feat: ensure the created guest doesn't have the presence confirmed by default

---
+++ src/domain/guest/guest.ts
@@ -0,0 +1,17 @@
+import { Person } from '../person/person';
+
+class Guest extends Person {
+  private confirmedPresence: boolean;
+
+  constructor(firstName: string, lastName: string, birthDate: Date) {
+    super(firstName, lastName, birthDate);
+
+    this.confirmedPresence = false;
+  }
+
+  public isPresenceConfirmed() {
+    return this.confirmedPresence;
+  }
+}
+
+export { Guest };
