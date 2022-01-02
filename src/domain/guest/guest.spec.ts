a5f939b5f20be05df4e87065cd6f36f016f83de6
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-01-02 11:33:08 -0300

    test: ensure the created guest doesn't have the presence confirmed by default

---
+++ src/domain/guest/guest.spec.ts
@@ -0,0 +1,24 @@
+import faker from 'faker';
+import { Guest } from './guest';
+
+type SystemUnderTestType = {
+  guest: Guest;
+};
+
+const buildSystemUnderTest = (): SystemUnderTestType => {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const birthDate = faker.date.between('01/01/1970', '01/01/2022');
+
+  const guest = new Guest(firstName, lastName, birthDate);
+
+  return { guest };
+};
+
+describe('Guest', () => {
+  test('Should be not confirmed presence by default', () => {
+    const { guest } = buildSystemUnderTest();
+
+    expect(guest.isPresenceConfirmed()).toBe(false);
+  });
+});
